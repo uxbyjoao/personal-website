9c755d1955bab4caf7d76cc206f3f705368fbeb2
João Gomes <jlfgomes@gmail.com>
2023-02-22 01:24:38 +0100

    trying without jwt

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,39 +1,50 @@
 import jwt from "jsonwebtoken";
 
 export async function post({ request }) {
-  const { password } = await request.json();
-  return new Response(
-    JSON.stringify({ message: "echoing password", password }),
-    { status: 200 }
-  );
-  // if (password === import.meta.env.CONTENT_PASSWORD) {
-  //   const token = jwt.sign(
-  //     { loggedIn: true },
-  //     import.meta.env.CONTENT_PRIVATE_KEY,
-  //     {
-  //       expiresIn: "24h",
-  //     }
-  //   );
-  //   return new Response(
-  //     JSON.stringify({
-  //       message: "Login successful!",
-  //       token,
-  //     }),
-  //     {
-  //       status: 200,
-  //       headers: {
-  //         "Set-Cookie": `auth_token=${token}; Path=/projects; HttpOnly`,
-  //       },
-  //     }
-  //   );
-  // } else {
-  //   return new Response(
-  //     JSON.stringify({
-  //       message: "Login failed!",
-  //     }),
-  //     {
-  //       status: 401,
-  //     }
-  //   );
-  // }
+  const body = await request.json();
+  const password = body.password;
+
+  // No password provided
+  if (!password) {
+    return new Response(
+      JSON.stringify({
+        message: "No password provided!",
+      }),
+      {
+        status: 400,
+      }
+    );
+  }
+
+  // Password is correct
+  if (password === import.meta.env.CONTENT_PASSWORD) {
+    // const token = jwt.sign(
+    //   { loggedIn: true },
+    //   import.meta.env.CONTENT_PRIVATE_KEY,
+    //   {
+    //     expiresIn: "24h",
+    //   }
+    // );
+    return new Response(
+      JSON.stringify({
+        message: "Login successful!",
+        password,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Set-Cookie": `auth_token=ABLUBLE; Path=/projects; HttpOnly`,
+        },
+      }
+    );
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: "Login failed!",
+      }),
+      {
+        status: 401,
+      }
+    );
+  }
 }
